5db04d40c55622bd43c64f186ef615475ecf3a7f
Your Name <you@example.com>
2024-04-24 10:53:31 +0530

    delete feature added

--- todos/components/todos.jsx
+++ todos/components/todos.jsx
@@ -1,19 +1,24 @@
+
 function TodosDis(props) {
-    // const Handledelete = (index) => {
-    //     const updatedTodos = [...props.todos];
-    //     updatedTodos.splice(index, 1);
-    //     props.setTodos(updatedTodos);
-    // };
+    const handleDelete = (index) => {
+        console.log("called");
+        const updatedTodos = [...props.todos];
+        updatedTodos.splice(index, 1);
+        props.setTodos(updatedTodos);
+        //console.log(abc);
+    };
+    
 
     return (
         <>
         {props.todos.map((todo, index) => (
             <div key={index}>
                 <h3>{todo}</h3>
-                {/* <button onClick={() => handleDelete(index)}>delete</button> */}
+                { <button onClick={() => handleDelete(index)}>delete</button> }
 
             </div>
         ))}
         </>
     );
 }
+export default TodosDis;
